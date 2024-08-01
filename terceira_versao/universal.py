0acfb255255c3de9355979e04019d7d79c42ac97
Joao Pedro Ramalho <jpv.ramalho2013@gmail.com>
2024-08-01 10:49:06 -0300

    [add] debug update_HP()

--- terceira_versao/universal.py
+++ terceira_versao/universal.py
@@ -190,8 +190,9 @@ def print_round_info(message):
 # Atualiza as suas vidas
 def update_HP(message):
     global PLAYERS_HPS, MY_ID
-    #print(f"[DEBUG] PLAYERS_HPS[{MY_ID}] no inicio da rodada: {PLAYERS_HPS[MY_ID]}")
+    print(f"[DEBUG] PLAYERS_HPS[{MY_ID}] no inicio da rodada: {PLAYERS_HPS[MY_ID]}")
     PLAYERS_HPS[MY_ID] = message["data"][2][MY_ID]
+    print(f"[DEBUG] PLAYERS_HPS[{MY_ID}] dps da rodada: {PLAYERS_HPS[MY_ID]}")
     print(f"HP: {PLAYERS_HPS[MY_ID]}")
 
 def check_players_alive():
