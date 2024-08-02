80406c5f2ebf5d9a379556a9beb7f8c6037b5d92
Joao Pedro Ramalho <jpv.ramalho2013@gmail.com>
2024-08-02 10:27:26 -0300

    [fix] parenteses errado em count_points

--- terceira_versao/universal.py
+++ terceira_versao/universal.py
@@ -271,7 +271,7 @@ def count_points():
             card_index = CARDS.index(move[1][0])
         except ValueError:
             card_index = -1 # O jogaor morreu, então a carta jogada (-1) não está no paralho
-        index_players.append(card_index))
+        index_players.append(card_index)
     
    #print(f"[DEBUG] index_players: {index_players}")
     
